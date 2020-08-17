99a87941a4f92b92307954e425626e64f3ee562f
Alexander G <alexandergosine92@gmail.com>
2020-08-17 17:06:11 -0500

    Added about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
      def home
           render html: 'Hello World!'
         end
+     def about
+          render html:  'About Page!' 
+     end 
 end
